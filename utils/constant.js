658e7834cdb1014bdb0d53515507a4bb5243dd5b
Paolo Gonzales <paologonzales1616@gmail.com>
2019-07-07 20:13:24 +0800

    Added constants

---
+++ utils/constant.js
@@ -0,0 +1,27 @@
+export const STATIONS = [1, 2, 4, 5, 8, 15, 16, 17, 18];
+
+export const MONTH_NAMES = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+export const FEATURES = [
+  "pH",
+  "ammonia",
+  "nitrate",
+  "inorganic_phosphate",
+  "BOD",
+  "dissolved_oxygen",
+  "fecal_coliforms",
+  "wqi"
+];

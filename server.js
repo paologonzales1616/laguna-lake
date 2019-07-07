2fa839a7cf7257860c18f6db873235ad813e5a05
Paolo Gonzales <paologonzales1616@gmail.com>
2019-07-07 16:43:56 +0800

    Initial server commit

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const next = require("next");
-const { PythonShell } = require("python-shell");
+const bodyParser = require("body-parser");
+// const { PythonShell } = require("python-shell");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
@@ -11,13 +12,52 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
-  server.get(["/", "/home"], async (req, res) => {
-    await PythonShell.run("main.py", {}, (err, results) => {
-      if (err) throw err;
-      // results is an array consisting of messages collected during execution
-      console.log("results: %j", results);
-      return app.render(req, res, "/index", { id: results });
-    });
+  server.use(bodyParser.json());
+  server.use(bodyParser.urlencoded({ extended: true }));
+
+  server.get("/api/login", (req, res) => {
+    res.json({ message: "Login API Route" });
+  });
+
+  server.get("/api/register", (req, res) => {
+    res.json({ message: "Register API Route" });
+  });
+
+  server.get("/forecast", (req, res) => {
+    return app.render(req, res, "/forecast", req.query);
+  });
+
+  server.get("/simulation", (req, res) => {
+    return app.render(req, res, "/timeline", req.query);
+  });
+
+  server.get("/timeline", (req, res) => {
+    return app.render(req, res, "/timeline", req.query);
+  });
+
+  server.get("/about", (req, res) => {
+    return app.render(req, res, "/about", req.query);
+  });
+
+  server.get("/contact", (req, res) => {
+    return app.render(req, res, "/contact", req.query);
+  });
+
+  server.get("/register", (req, res) => {
+    return app.render(req, res, "/register", req.query);
+  });
+
+  server.get("/login", (req, res) => {
+    return app.render(req, res, "/login", req.query);
+  });
+
+  server.get(["/home", "/"], (req, res) => {
+    // await PythonShell.run("main.py", {}, (err, results) => {
+    //   if (err) throw err;
+    //   // results is an array consisting of messages collected during execution
+    //   console.log("results: %j", results);
+    return app.render(req, res, "/index", req.query);
+    // });
   });
 
   server.get("*", (req, res) => {
@@ -26,6 +66,6 @@ app.prepare().then(() => {
 
   server.listen(port, err => {
     if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+    console.log(`Ready on http://localhost:${port}`);
   });
 });

7e21c280f9306ef5b59394daf103daa9d0eab979
Paolo Gonzales <paologonzales1616@gmail.com>
2019-07-09 02:20:29 +0800

    Mapbox css

--- components/Head.js
+++ components/Head.js
@@ -8,6 +8,10 @@ const Head = () => {
         rel="stylesheet"
         href="https://bootswatch.com/4/yeti/bootstrap.min.css"
       />
+      <link
+        href="https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css"
+        rel="stylesheet"
+      />
     </NextHead>
   );
 };

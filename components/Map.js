95d6abf4254b4fe751ff7565d91834cd87f0af6c
Paolo Gonzales <paologonzales1616@gmail.com>
2019-07-15 07:47:31 +0800

    Replace existing mapbox library

--- components/Map.js
+++ components/Map.js
@@ -1,42 +1,26 @@
-import React, { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import React from "react";
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+
+const Mapbox = ReactMapboxGl({
+  accessToken:
+    "pk.eyJ1IjoicGFkbzY5IiwiYSI6ImNqc2xiMHMxcjJqZmQ0M3M3bDhpM21tbW8ifQ.ucrihizFRCj9M70JR7hmDg"
+});
 
 const Map = () => {
-  const [viewport, setViewport] = useState({
-    latitude: 14.367071317719422,
-    longitude: 121.23386808788138,
-    zoom: 10.2,
-    width: "100%",
-    height: "100%"
-  });
-
   return (
-    <>
-      <div className="col-9 px-0">
-        <div className="map-wrapper">
-          <ReactMapGL
-            mapStyle="mapbox://styles/mapbox/outdoors-v11"
-            mapboxApiAccessToken="pk.eyJ1IjoicGFkbzY5IiwiYSI6ImNqc2xiMHMxcjJqZmQ0M3M3bDhpM21tbW8ifQ.ucrihizFRCj9M70JR7hmDg"
-            onViewportChange={viewport => setViewport(viewport)}
-            {...viewport}
-            style={{
-              height: "100%",
-              width: "100%"
-            }}
-          />
-        </div>
-        <style jsx>{`
-          .col-9 {
-            height: 100%;
-          }
-          .map-wrapper {
-            position: fixed;
-            width: 100%;
-            height: 100%;
-          }
-        `}</style>
-      </div>
-    </>
+    <Mapbox
+      center={[-0.481747846041145, 51.3233379650232]}
+      style="mapbox://styles/mapbox/streets-v9"
+      zoom={[10.2]}
+      containerStyle={{
+        height: "94vh",
+        width: "100%"
+      }}
+    >
+      <Layer type="symbol" layout={{ "icon-image": "marker-15" }}>
+        <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+      </Layer>
+    </Mapbox>
   );
 };
 

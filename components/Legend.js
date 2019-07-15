233a0098591c683a90d7f7d0bcf72726e4dc29f3
Paolo Gonzales <paologonzales1616@gmail.com>
2019-07-15 21:13:43 +0800

    Dynamic Legend

---
+++ components/Legend.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { Table } from "reactstrap";
+const headers = {
+  Accept: "application/json",
+  "Content-Type": "application/json"
+};
+
+const Legend = props => {
+  const [colors, setColors] = useState([]);
+
+  const fetchColors = async () => {
+    const optionsLegend = {
+      headers: headers,
+      method: "POST",
+      body: JSON.stringify({
+        feature: props.legend
+      })
+    };
+
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/legend`,
+        optionsLegend
+      );
+      const data = await response.json();
+      setColors(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchColors();
+  }, []);
+
+  return (
+    <Table bordered className="p-0 m-0" style={{ fontSize: "12px" }}>
+      <thead>
+        <tr>
+          <th>
+            <center>INDICATOR</center>
+          </th>
+          <th>
+            <center>RANGE</center>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {colors.map((data, index) => (
+          <tr key={index}>
+            <td style={{ backgroundColor: data.color, padding: 8 }}>
+              <></>
+            </td>
+            <td>
+              <center>
+                <b>{`${data.min} - ${data.max}`}</b>
+              </center>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+export default Legend;

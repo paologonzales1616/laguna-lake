07dea3568fea8898842dcc84f66babb315380dd7
Paolo Gonzales <paologonzales1616@gmail.com>
2019-07-15 21:13:29 +0800

    Fixed layout

--- components/Layout.js
+++ components/Layout.js
@@ -3,17 +3,34 @@ import Navigation from "./Navigation";
 
 const Layout = props => {
   return (
-    <>
+    <div className="box">
       <Head />
-      <Navigation />
-      {props.children}
+      <div className="header">
+        <Navigation />
+      </div>
+      <div className="main">{props.children}</div>
       <style global jsx>{`
         body,
         html {
-          height: 100%;
+          margin: 0;
+        }
+        .box {
+          height: 100vh;
+          margin: 0;
+          display: grid;
+          grid-template-columns: 1fr;
+          grid-template-rows: auto 1fr;
+        }
+
+        .header {
+          grid-column: 1 / span 5;
+        }
+
+        .main {
+          grid-column: 1 / span 5;
         }
       `}</style>
-    </>
+    </div>
   );
 };
 

62068d8add4e2f20565651ce366145019d11f981
Paolo Gonzales <paologonzales1616@gmail.com>
2019-07-15 21:40:41 +0800

    Use Isomorphic Unfetch

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Layout from "../components/Layout";
 import dynamic from "next/dynamic";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import moment from "moment";
 import {
   Row,

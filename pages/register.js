904ba3002b6f03d6946c383b52980ec2269d05d7
Paolo Gonzales <paologonzales1616@gmail.com>
2019-07-29 14:04:13 +0800

    Login and register form

--- pages/register.js
+++ pages/register.js
@@ -1,11 +1,63 @@
 import Layout from "../components/Layout";
-
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Container,
+  Row,
+  Col,
+  Button
+} from "reactstrap";
 const Register = () => {
-    return (
-        <Layout>
-            Register
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <Container>
+        <Row>
+          <Col sm={{ size: 12 }} md={{ size: 6, offset: 3 }}>
+            <div className="panel">
+              <h1 className="text-center">Register</h1>
+              <Form>
+                <FormGroup>
+                  <Label for="exampleEmail">Name</Label>
+                  <Input type="email" placeholder="Enter Name" />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="exampleEmail">Email</Label>
+                  <Input type="email" placeholder="Enter Email Address" />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="examplePassword">Password</Label>
+                  <Input type="password" placeholder="Enter Password" />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="examplePassword">Confirm Password</Label>
+                  <Input type="password" placeholder="Enter Password Again" />
+                </FormGroup>
+              </Form>
+              <Button block color="primary">
+                Register
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <style jsx>{`
+        .panel {
+          background: #fff;
+          box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
+          padding: 20px 24px;
+          margin: 20px;
+          margin-top: 3rem;
+          font-size: 13px;
+          line-height: 2;
+          color: #6b6b76;
+          outline: none;
+          text-transform: uppercase;
+        }
+      `}</style>
+    </Layout>
+  );
+};
 
-export default Register
+export default Register;
--- utils/pdfContainer.js
+++ utils/pdfContainer.js
@@ -4,17 +4,15 @@ export default props => {
   const bodyRef = React.createRef();
   const createPdf = () => props.createPdf(bodyRef.current);
   return (
-    <>
-      <section className="pdf-container">
-        <section className="pdf-toolbar">
-          <Button color="link" onClick={createPdf}>
-            Create PDF
-          </Button>
-        </section>
-        <section className="pdf-body" ref={bodyRef}>
-          {props.children}
-        </section>
+    <section className="pdf-container">
+      <section className="pdf-toolbar">
+        <Button color="link" onClick={createPdf}>
+          Create PDF
+        </Button>
       </section>
-    </>
+      <section className="pdf-body" ref={bodyRef}>
+        {props.children}
+      </section>
+    </section>
   );
 };

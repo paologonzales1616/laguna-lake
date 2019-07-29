cdcd8cc10665ee6ac316f29aee0f47b4ee7eb444
Paolo Gonzales <paologonzales1616@gmail.com>
2019-07-29 18:12:31 +0800

    database config

---
+++ configs/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  url: "mongodb://localhost:27017/lake" // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+  //Please replace your host file Here : 127.1.1.0 , Express is Collection Name (Database Name)
+};
